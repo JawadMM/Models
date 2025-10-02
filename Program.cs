2ab7b7e00c0e48095849b63ae6013a85e5685254
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-10-03 02:43:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Models
+{
+
+  internal class Program
+  {
+
+    public class Computer
+    {
+      private string? Motherboard { get; set; }
+      private int CPUCores { get; set; }
+      private bool HasWiFi { get; set; }
+      private bool HasLTE { get; set; }
+      private decimal Price { get; set; }
+      private string? VideoCard { get; set; }
+    }
+    static void main(string[] args)
+    {
+
+    }
+  }
+}
