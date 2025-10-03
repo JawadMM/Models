df529162417dff26160328ab22587c8e5dfb026c
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-10-03 03:00:19 +0300

    fixed syntax issue

--- Program.cs
+++ Program.cs
@@ -3,22 +3,49 @@ using System.Text.RegularExpressions;
 
 namespace Models
 {
+  public class Computer
+  {
+    public string Motherboard { get; set; }
+    public int CPUCores { get; set; }
+    public bool HasWiFi { get; set; }
+    public bool HasLTE { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public decimal Price { get; set; }
+    public string VideoCard { get; set; }
 
+    public Computer()
+    {
+      if (Motherboard == null)
+      {
+        Motherboard = "";
+      }
+      if (VideoCard == null)
+      {
+        VideoCard = "";
+      }
+    }
+  }
   internal class Program
   {
-
-    public class Computer
+    static void Main(string[] args)
     {
-      private string? Motherboard { get; set; }
-      private int CPUCores { get; set; }
-      private bool HasWiFi { get; set; }
-      private bool HasLTE { get; set; }
-      private decimal Price { get; set; }
-      private string? VideoCard { get; set; }
-    }
-    static void main(string[] args)
-    {
+      Computer newComputer = new Computer()
+      {
+        Motherboard = "ASUS",
+        HasWiFi = true,
+        HasLTE = false,
+        ReleaseDate = DateTime.Now,
+        Price = 944.44m,
+        VideoCard = "RTX",
+      };
 
+      Console.WriteLine(newComputer.Motherboard);
+      Console.WriteLine(newComputer.HasWiFi);
+      newComputer.HasWiFi = false;
+      Console.WriteLine(newComputer.HasWiFi);
+      Console.WriteLine(newComputer.Price);
+      Console.WriteLine(newComputer.ReleaseDate);
+      Console.WriteLine(newComputer.VideoCard);
     }
   }
 }
